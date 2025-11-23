e3eb05279f2766f351cce6c64c408474b6203ac9
Manjeetsinh Alonja <alonjamohit77@gmail.com>
2025-11-22 20:55:30 -0500

    added submodule

--- main.tf
+++ main.tf
@@ -52,15 +52,61 @@ resource "aws_iam_policy" "ddb_put_policy" {
   policy = data.aws_iam_policy_document.ddb_put.json
 }
 
+# DynamoDB Read permissions (GetItem/Query/Scan)
+data "aws_iam_policy_document" "ddb_read" {
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+    resources = [aws_dynamodb_table.users.arn]
+  }
+}
+
+resource "aws_iam_policy" "ddb_read_policy" {
+  name   = "${var.project}-ddb-read"
+  policy = data.aws_iam_policy_document.ddb_read.json
+}
+
+# Attach custom policies to Lambda execution role
+resource "aws_iam_role_policy_attachment" "ddb_put_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.ddb_put_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ddb_read_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.ddb_read_policy.arn
+}
+
 # Lambda function
 resource "aws_lambda_function" "user_writer" {
   function_name = "${var.project}-create-user"
   role          = aws_iam_role.lambda_exec_role.arn
   runtime       = var.lambda_runtime
   handler       = "index.handler"
-  filename      = "${path.module}/lambda/create-user/build.zip"
+  filename      = "${path.module}/sample-project/lambda/create-user/build.zip"
   # Ensure Terraform detects changes to the lambda package by hashing the zip
-  source_code_hash = filebase64sha256("${path.module}/lambda/create-user/build.zip")
+  source_code_hash = filebase64sha256("${path.module}/sample-project/lambda/create-user/build.zip")
+  memory_size      = var.lambda_memory_mb
+  timeout          = var.lambda_timeout_s
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.users.name
+    }
+  }
+}
+
+# Reader Lambda function (GET user)
+resource "aws_lambda_function" "user_reader" {
+  function_name    = "${var.project}-get-user"
+  role             = aws_iam_role.lambda_exec_role.arn
+  runtime          = var.lambda_runtime
+  handler          = "index.handler"
+  filename         = "${path.module}/sample-project/lambda/get-user/build.zip"
+  source_code_hash = filebase64sha256("${path.module}/sample-project/lambda/get-user/build.zip")
   memory_size      = var.lambda_memory_mb
   timeout          = var.lambda_timeout_s
 
@@ -88,7 +134,29 @@ resource "aws_cloudwatch_log_group" "lambda_lg" {
   retention_in_days = 14
 }
 
+# Log group for reader lambda
+resource "aws_cloudwatch_log_group" "lambda_reader_lg" {
+  name              = "/aws/lambda/${aws_lambda_function.user_reader.function_name}"
+  retention_in_days = 14
+}
+
 output "lambda_function_url" {
   value       = aws_lambda_function_url.fn_url.function_url
   description = "Public Function URL for POST requests"
+}
+
+# Public URL for get-user Lambda
+resource "aws_lambda_function_url" "reader_fn_url" {
+  function_name      = aws_lambda_function.user_reader.arn
+  authorization_type = "NONE"
+  cors {
+    allow_origins = ["*"]
+    allow_methods = ["*"]
+    allow_headers = ["*"]
+  }
+}
+
+output "get_user_function_url" {
+  value       = aws_lambda_function_url.reader_fn_url.function_url
+  description = "Public Function URL for GET/lookup requests"
 }
